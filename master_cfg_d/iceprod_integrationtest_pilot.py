624a96ced290ed6f7d28e77093dc445b24e6b179
David Schultz <davids24@gmail.com>
2017-12-06 13:29:35 -0600

    add integration test using pilots

---
+++ master_cfg_d/iceprod_integrationtest_pilot.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+
+import os
+import json
+
+from buildbot.plugins import *
+
+from . import Config, get_os
+
+# depends on icperod_setup
+from .iceprod_setup import config as iceprod_setup_config
+
+
+prefix = __file__.split('/')[-1].rsplit('.',1)[0]
+
+
+def setup(cfg):
+
+    ####### WORKERS
+
+    workername = 'iceprod-condor-centos7'
+    cfg['workers'][prefix+'_worker'] = worker.Worker(
+        workername, os.environ['WORKER_PASSWORD'],
+        max_builds=1,
+    )
+
+
+    ####### CHANGESOURCES
+
+
+    ####### BUILDERS
+
+    path = '/shared/iceprod'
+
+    factory = util.BuildFactory()
+    # start iceprod server
+    factory.addStep(steps.Git(
+        repourl='git://github.com/WIPACrepo/iceprod.git',
+        mode='full',
+        method='clobber',
+    ))
+    factory.addStep(steps.ShellCommand(
+        name='integration test',
+        command=[
+            os.path.join(path,'iceprod/master/env-shell.sh'),
+            'python','-m','integration_tests','--pilot',
+        ],
+        locks=[
+            cfg.locks['gpu'].access('counting'),
+            cfg.locks['iceprod_shared'].access('counting'),
+        ],
+    ))
+
+    cfg['builders'][prefix+'_builder'] = util.BuilderConfig(
+        name=prefix+'_builder',
+        workername=workername,
+        factory=factory,
+        properties={},
+    )
+
+
+    ####### SCHEDULERS
+
+    cfg['schedulers'][prefix+'-dep'] = schedulers.Triggerable(
+        name=prefix+'-dep',
+        builderNames=[prefix+'_builder'],
+    )
+
+# a fully dependent config
+config = None
+iceprod_setup_config.register_dependency(Config(setup))
